cbda5c5776f82b44351034617f3af17b1819ff94
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-02-24 12:00:56 +0800

    change layout2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import TopBar from './global/topbar';  // Add this import
 import Create from './pages/Create';  // Add this import
 
-const client = generateClient<Schema>();
 
 function App() {
   return (
