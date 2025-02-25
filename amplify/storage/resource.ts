3cd8efe94294af1e682b2a01138f91d90e3e5cd9
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-02-25 15:34:22 +0800

    get

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,16 +1,12 @@
 import { defineStorage } from '@aws-amplify/backend';
-import { textract } from '../functions/textract/resource';
+
 
 export const storage = defineStorage({
   name: 'UserStorages',
-  triggers: {
-    onUpload: textract, // Use the textract function as the trigger
-  },
   access: (allow) => ({
     'private-images/{entity_id}/*': [
       allow.guest.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete']),
-      allow.resource(textract).to(['read', 'write', 'delete'])
+      allow.entity('identity').to(['read', 'write', 'delete'])
     ],
     'image-documents/*': [
       allow.authenticated.to(['read','write', 'delete']),
